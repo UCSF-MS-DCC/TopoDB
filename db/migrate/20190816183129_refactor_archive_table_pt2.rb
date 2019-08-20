5f54a05b47c69ca073b106f5e87fa6c32b1e717c
urrik98 <adamrenschen@gmail.com>
2019-08-20 15:09:49 -0700

    ...

--- db/migrate/20190816183129_refactor_archive_table_pt2.rb
+++ db/migrate/20190816183129_refactor_archive_table_pt2.rb
@@ -1,7 +1,7 @@
 class RefactorArchiveTablePt2 < ActiveRecord::Migration[5.2]
   def change
-    change_column :archives, :cage, :integer
-    change_column :archives, :mouse, :integer
-    change_column :archives, :who, :integer
+    change_column :archives, :cage, 'integer USING cage::integer'
+    change_column :archives, :mouse, 'integer USING mouse::integer'
+    change_column :archives, :who, 'integer USING who::integer'
   end
 end
