dd8deb457ad43d490d3134dfad23272569f9c187
urrik98 <adamrenschen@gmail.com>
2019-08-20 12:29:33 -0700

    Refactor migrations files to work with postgres limitations

--- db/migrate/20190725234249_change_cage_type_to_enum.rb
+++ db/migrate/20190725234249_change_cage_type_to_enum.rb
@@ -1,5 +1,5 @@
 class ChangeCageTypeToEnum < ActiveRecord::Migration[5.2]
   def change
-    change_column :cages, :cage_type, :integer
+    change_column :cages, :cage_type, 'integer USING cage_type::integer' 
   end
 end
--- db/migrate/20190725234615_change_cage_location_to_enum.rb
+++ db/migrate/20190725234615_change_cage_location_to_enum.rb
@@ -1,5 +1,5 @@
 class ChangeCageLocationToEnum < ActiveRecord::Migration[5.2]
   def change
-    change_column :cages, :location, :integer
+    change_column :cages, :location, 'integer USING cage_type::integer'
   end
 end
