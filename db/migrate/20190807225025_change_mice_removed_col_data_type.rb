4f1c2e058c7759529d1eb33aa9d59d3b159f3c43
urrik98 <adamrenschen@gmail.com>
2019-08-20 12:40:49 -0700

    ...

--- db/migrate/20190807225025_change_mice_removed_col_data_type.rb
+++ db/migrate/20190807225025_change_mice_removed_col_data_type.rb
@@ -1,5 +1,5 @@
 class ChangeMiceRemovedColDataType < ActiveRecord::Migration[5.2]
   def change
-    change_column :mice, :removed, 'TYPE date'
+    change_column :mice, :removed, 'date'
   end
 end
