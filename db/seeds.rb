7c0fc3abd14ef2fdd2cd21e2d2c2ccd25ce9b6b4
urrik98 <adamrenschen@gmail.com>
2019-10-29 12:07:13 -0700

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ end
         
         ep1 = Faker::Number.within(range: 2..10)
         ep2 = Faker::Number.within(range: 2..10)
-        types = %w(N R L RR RL RR RRL RLL RRLL)
+        types = %w(N R L RR RL LL RRL RLL RRLL)
         dbs = [Faker::Date.between(from: 1.year.ago, to: 6.months.ago), Faker::Date.between(from: 1.year.ago, to: 6.months.ago)]
         mouse1 = Mouse.new(:cage_id => cage.id, :sex => 2, :genotype => Faker::Number.within(range: 2..4), :dob => dbs[0] , :weaning_date => dbs[0] + 21, :three_digit_code => dgn,
                  :tail_cut_date => dbs[0] + 12, :ear_punch => ep1 , :designation => "M#{dgn}#{types[ep1 - 2]}", :tdc_generated => Time.now, :strain => cage.strain, :removed => nil ).save
@@ -56,13 +56,13 @@ end
         
         ep1 = Faker::Number.within(range: 2..10)
         ep2 = Faker::Number.within(range: 2..10)
-        types = %w(N R L RR RL RR RRL RLL RRLL)
+        types = %w(N R L RR RL LL RRL RLL RRLL)
         dbs = [Faker::Date.between(from: 1.year.ago, to: 6.months.ago), Faker::Date.between(from: 1.year.ago, to: 6.months.ago)]
         mouse1 = Mouse.new(:cage_id => cage.id, :sex => 2, :genotype => Faker::Number.within(range: 2..4), :genotype2 => Faker::Number.within(range: 2..4), :dob => dbs[0] , :weaning_date => dbs[0] + 21, :three_digit_code => dgn,
-                 :tail_cut_date => dbs[0] + 12, :ear_punch => ep1 , :designation => "M#{dgn}#{types[ep1 - 1]}", :tdc_generated => Time.now, :strain => st1, :strain2 => st2, :removed => nil ).save
+                 :tail_cut_date => dbs[0] + 12, :ear_punch => ep1 , :designation => "M#{dgn}#{types[ep1 - 2]}", :tdc_generated => Time.now, :strain => st1, :strain2 => st2, :removed => nil ).save
         dgn += 1
         mouse2 = Mouse.new(:cage_id => cage.id, :sex => 1, :genotype => Faker::Number.within(range: 2..4), :genotype2 => Faker::Number.within(range: 2..4), :dob => dbs[1] , :weaning_date => dbs[1] + 21, :three_digit_code => dgn,
-                 :tail_cut_date => dbs[1] + 12, :ear_punch => ep2, :designation => "F#{dgn}#{types[ep2 - 1]}", :tdc_generated => Time.now, :strain => st1, :strain2 => st2, :removed => nil ).save
+                 :tail_cut_date => dbs[1] + 12, :ear_punch => ep2, :designation => "F#{dgn}#{types[ep2 - 2]}", :tdc_generated => Time.now, :strain => st1, :strain2 => st2, :removed => nil ).save
         dgn += 1
     end
 end
@@ -77,13 +77,13 @@ dgn = 200
 
     cage = Cage.new(cage_number:Faker::Number.between(from: 200000, to: 250000), cage_type:ct, strain:st, location:ln, in_use:true)
     if cage.save
-        types = %w(N R L RR RL RR RRL RLL RRLL)
+        types = %w(N R L RR RL LL RRL RLL RRLL)
 
         Faker::Number.between(from:6, to:12).times do 
             db = Faker::Date.between(from:2.years.ago, to:3.months.ago)
             ep1 = Faker::Number.within(range: 2..10)
             Mouse.new(:cage_id => cage.id, :sex => sx == "f" ? 1 : 2, :genotype => Faker::Number.within(range: 2..4), :dob => db , :weaning_date => db + 21, :three_digit_code => dgn,
-                    :tail_cut_date => db + 12, :ear_punch => ep1 , :designation => "#{sx.upcase}#{dgn}#{types[ep1 - 1]}", :tdc_generated => Time.now, :strain => cage.strain, :removed => nil ).save
+                    :tail_cut_date => db + 12, :ear_punch => ep1 , :designation => "#{sx.upcase}#{dgn}#{types[ep1 - 2]}", :tdc_generated => Time.now, :strain => cage.strain, :removed => nil ).save
             dgn += 1
         end 
     end
@@ -98,14 +98,14 @@ end
 
     cage = Cage.new(cage_number:Faker::Number.between(from: 300000, to: 350000), cage_type:ct, strain:st, location:ln, in_use:true, genotype:gt)
     if cage.save
-        types = %w(N R L RR RL RR RRL RLL RRLL)
+        types = %w(N R L RR RL LL RRL RLL RRLL)
 
         Faker::Number.between(from:6, to:12).times do 
             ep1 = Faker::Number.within(range: 2..10)
             mths = Faker::Number.between(from:2, to:24)
             db = Faker::Date.between(from:mths.months.ago, to:(mths - 1).months.ago)
             Mouse.new(:cage_id => cage.id, :sex => sx == "f" ? 1 : 2, :genotype => gt, :dob => db , :weaning_date => db + 21, :three_digit_code => dgn,
-                    :tail_cut_date => db + 12, :ear_punch => ep1 , :designation => "#{sx.upcase}#{dgn}#{types[ep1 - 1]}", :tdc_generated => Time.now, :strain => cage.strain, :removed => nil ).save
+                    :tail_cut_date => db + 12, :ear_punch => ep1 , :designation => "#{sx.upcase}#{dgn}#{types[ep1 - 2]}", :tdc_generated => Time.now, :strain => cage.strain, :removed => nil ).save
             dgn += 1
         end 
     end
