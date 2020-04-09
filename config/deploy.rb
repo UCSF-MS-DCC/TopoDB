03d3522739596d6cb25add62c150a9f87804769b
urrik98 <adamrenschen@gmail.com>
2020-04-09 10:26:07 -0700

    Remove unneeded migration file

--- config/deploy.rb
+++ config/deploy.rb
@@ -94,8 +94,9 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      #unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      unless `git rev-parse HEAD` == `git rev-parse origin/uat`
+        puts "WARNING: HEAD is not the same as origin/uat"
         puts "Run `git push` to sync changes."
         exit
       end
--- db/migrate/20200326171213_rename_experiment_mouses_to_datapoints.rb
+++
@@ -1,5 +0,0 @@
-class RenameExperimentMousesToDatapoints < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :experiment_mouses
-  end
-end
