abcf4634203a7019ad54f6d35344b74d5dd61445
urrik98 <adamrenschen@gmail.com>
2020-04-23 17:29:13 -0700

    Update config deployment files to fix capistrano ignoring revisions.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,6 +11,39 @@ set :stage,         :staging
 set :application,   "uat-topodb"
 set :deploy_to,     "/var/www/#{fetch(:application)}"
 
+  namespace :deploy do
+    desc "Make sure local git is in sync with remote."
+    task :check_revision do
+      on roles(:app) do
+        #unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        unless `git rev-parse HEAD` == `git rev-parse origin/uat`
+          puts "WARNING: HEAD is not the same as origin/uat"
+          puts "Run `git push` to sync changes."
+          exit
+        end
+      end
+    end
+  
+    desc 'Initial Deploy'
+    task :initial do
+      on roles(:app) do
+        before 'deploy:restart', 'puma:start'
+        invoke 'deploy'
+      end
+    end
+  
+    desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+    end
+  
+    before :starting,     :check_revision
+    after  :finishing,    :compile_assets
+    after  :finishing,    :cleanup
+    # after  :finishing,    :restart
+  end
 
 # role-based syntax
 # ==================
