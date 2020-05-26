aa153f2a981ccdccf5fdfa6ba3d57bfd47b0ee01
urrik98 <adamrenschen@gmail.com>
2020-05-26 15:55:22 -0700

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,39 +51,39 @@ set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle publi
 #   before :start, :make_dirs
 # end
 
-namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
-#     on roles(:app) do
-#       #unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#       unless `git rev-parse HEAD` == `git rev-parse origin/uat`
-#         puts "WARNING: HEAD is not the same as origin/uat"
-#         puts "Run `git push` to sync changes."
-#         exit
-#       end
+# namespace :deploy do
+# #   desc "Make sure local git is in sync with remote."
+# #   task :check_revision do
+# #     on roles(:app) do
+# #       #unless `git rev-parse HEAD` == `git rev-parse origin/master`
+# #       unless `git rev-parse HEAD` == `git rev-parse origin/uat`
+# #         puts "WARNING: HEAD is not the same as origin/uat"
+# #         puts "Run `git push` to sync changes."
+# #         exit
+# #       end
+# #     end
+# #   end
+
+# #   desc 'Initial Deploy'
+# #   task :initial do
+# #     on roles(:app) do
+# #       before 'deploy:restart', 'puma:start'
+# #       invoke 'deploy'
+# #     end
+# #   end
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       invoke 'puma:restart'
 #     end
 #   end
 
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-#   # before :starting,     :check_revision
-#   after  :finishing,    :compile_assets
-#   after  :finishing,    :cleanup
-#   # after  :finishing,    :restart
-end
+# #   # before :starting,     :check_revision
+# #   after  :finishing,    :compile_assets
+# #   after  :finishing,    :cleanup
+# #   # after  :finishing,    :restart
+# end
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,8 +10,51 @@ set :branch,        :master
 set :stage,         :production
 set :application,   "topodb"
 set :deploy_to,     "/var/www/#{fetch(:application)}"
-
-
+set :puma_bind,     "unix:///var/www/topodb/tmp/sockets/topodb-puma.sock"
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+  before :start, :make_dirs
+end
+namespace :deploy do
+    #   desc "Make sure local git is in sync with remote."
+    #   task :check_revision do
+    #     on roles(:app) do
+    #       #unless `git rev-parse HEAD` == `git rev-parse origin/master`
+    #       unless `git rev-parse HEAD` == `git rev-parse origin/uat`
+    #         puts "WARNING: HEAD is not the same as origin/uat"
+    #         puts "Run `git push` to sync changes."
+    #         exit
+    #       end
+    #     end
+    #   end
+    
+    #   desc 'Initial Deploy'
+    #   task :initial do
+    #     on roles(:app) do
+    #       before 'deploy:restart', 'puma:start'
+    #       invoke 'deploy'
+    #     end
+    #   end
+    
+      desc 'Restart application'
+      task :restart do
+        on roles(:app), in: :sequence, wait: 5 do
+          invoke 'puma:restart'
+        end
+      end
+    
+    #   # before :starting,     :check_revision
+    #   after  :finishing,    :compile_assets
+    #   after  :finishing,    :cleanup
+    #   # after  :finishing,    :restart
+    end
+    
 # role-based syntax
 # ==================
 
